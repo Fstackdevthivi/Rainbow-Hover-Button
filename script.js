f0c51fd194957290c5e6dccb46bb3f368686fbb1
Thiviyan <fstackdevthivi@gmail.com>
2024-12-11 17:05:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+$(function () {
+    $(".rainbow-hover").hover(
+        function () {
+            // Lors du survol
+            $('.sp').css({
+                "background": "#2B3044",
+                "-webkit-background-clip": "text",
+                "-webkit-text-fill-color": "transparent",
+                "background-clip": "text",
+                "text-fill-color": "transparent",
+                "display": "block"
+            });
+        },
+        function () {
+            // Lorsque le survol est terminé
+            $('.sp').css({
+                "background": "linear-gradient(90deg, #866ee7, #ea60da, #ed8f57, #fbd41d, #2cca91)",
+                "-webkit-background-clip": "text",
+                "-webkit-text-fill-color": "transparent",
+                "background-clip": "text",
+                "text-fill-color": "transparent",
+                "display": "block"
+            });
+        }
+    );
+});
+/*$(function () {
+    $(".rainbow-hover").hover(
+        function () {
+            // Lors du survol, ajouter la classe d'animation
+            $('.sp').addClass('animate-hover');
+        },
+        function () {
+            // Lorsque le survol est terminé, retirer la classe d'animation
+            $('.sp').removeClass('animate-hover');
+        }
+    );
+});*/
